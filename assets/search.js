acd394d826cf5b14419346548bfc06069e9abb7a
obsidianubg <nightiumubg@gmail.com>
2025-09-03 19:37:03 -0400

    fix search

--- assets/search.js
+++ assets/search.js
@@ -1,12 +1,13 @@
 const searchBar = document.getElementById("searchBar");
-const games = document.querySelectorAll(".game-grid a");
 
 searchBar.addEventListener("keyup", function() {
     const searchText = this.value.toLowerCase();
+    const games = document.querySelectorAll(".game-grid a"); 
+    
     games.forEach(game => {
         const altText = game.querySelector("img").alt.toLowerCase();
         if (altText.includes(searchText)) {
-            game.style.display = "inline-block";
+            game.style.display = ""; 
         } else {
             game.style.display = "none";
         }
